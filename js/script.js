361d62ee87de680c43897f4bb0344da63285448d
Will Hui <will@willhui.com>
2016-08-21 00:16:03 +0800

    init

---
+++ js/script.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+
+  var $black    = '#0A0908';
+  var $white    = '#F7F7F7';
+  var $darkGrey = '#494949';
+  var $grey     = '#7C7A7A';
+  var $rose     = '#FF5D73';
+
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: '#menu',
+        lockAnchors: false,
+        anchors:['home', 'about', 'skills', 'portfolio', 'contact'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: [],
+        showActiveTooltip: false,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: true,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: true,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: false,
+        resetSliders: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        sectionsColor : [$black, $darkGrey, $white, $rose, $grey],
+        fixedElements: '#header, .footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){
+          new WOW().init();
+        },
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+
+
+
+
+
+
+
+    });
+
+})
