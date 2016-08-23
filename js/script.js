c3992c4cd509fea2a214e29662c66fa4970a099f
Will Hui <will@willhui.com>
2016-08-23 17:49:33 +0800

    fixed media queries for about page

--- js/script.js
+++ js/script.js
@@ -12,133 +12,48 @@ $(document).ready(function() {
   var normalConfig = {
         //Navigation
         menu: '#menu',
-        lockAnchors: false,
         anchors:['home', 'about', 'portfolio', 'contact'],
-        navigation: false,
-        navigationPosition: 'right',
-        navigationTooltips: [],
-        showActiveTooltip: false,
         slidesNavigation: true,
         slidesNavPosition: 'bottom',
 
         //Scrolling
         css3: true,
-        scrollingSpeed: 700,
-        autoScrolling: true,
-        fitToSection: true,
         fitToSectionDelay: 1000,
         scrollBar: true,
-        easing: 'easeInOutCubic',
-        easingcss3: 'ease',
-        loopBottom: true,
-        loopTop: false,
-        loopHorizontal: true,
-        continuousVertical: false,
-        continuousHorizontal: false,
-        scrollHorizontally: true,
-        interlockedSlides: false,
-        resetSliders: false,
-        normalScrollElements: '#element1, .element2',
-        scrollOverflow: false,
-        scrollOverflowOptions: null,
         touchSensitivity: 100,
-        normalScrollElementTouchThreshold: 5,
-        bigSectionsDestination: null,
-
-        //Accessibility
-        keyboardScrolling: true,
-        animateAnchor: true,
-        recordHistory: true,
 
         //Design
         controlArrows: false,
-        verticalCentered: true,
         paddingTop: '3em',
         paddingBottom: '10px',
         sectionsColor : [$black, $white, $grey, $black],
         fixedElements: '#header, .footer',
-        // responsiveWidth: 479,
-        // responsiveHeight: 600,
 
         //Custom selectors
         sectionSelector: '.section',
         slideSelector: '.slide',
 
         //events
-        onLeave: function(index, nextIndex, direction){},
-        afterLoad: function(anchorLink, index){},
         afterRender: function(){
           new WOW().init();
         },
-        afterResize: function(){},
-        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
     };
 
   var smlScreenConfig = {
         //Navigation
         menu: '#menu',
-        lockAnchors: false,
         anchors:['home', 'about', 'portfolio', 'contact'],
-        navigation: false,
-        navigationPosition: 'right',
-        navigationTooltips: [],
-        showActiveTooltip: false,
-        slidesNavigation: true,
-        slidesNavPosition: 'bottom',
+        slidesNavigation: false,
 
         //Scrolling
         css3: true,
-        scrollingSpeed: 700,
-        autoScrolling: false,
-        fitToSection: true,
-        fitToSectionDelay: 1000,
-        scrollBar: false,
-        easing: 'easeInOutCubic',
-        easingcss3: 'ease',
         loopBottom: true,
-        loopTop: false,
-        loopHorizontal: true,
-        continuousVertical: false,
-        continuousHorizontal: false,
-        scrollHorizontally: true,
-        interlockedSlides: false,
-        resetSliders: false,
-        normalScrollElements: '#element1, .element2',
-        scrollOverflow: false,
-        scrollOverflowOptions: null,
+        scrollOverflow: true,
         touchSensitivity: 100,
-        normalScrollElementTouchThreshold: 5,
-        bigSectionsDestination: null,
 
-        //Accessibility
-        keyboardScrolling: true,
-        animateAnchor: true,
-        recordHistory: true,
-
-        //Design
-        controlArrows: false,
-        verticalCentered: true,
         paddingTop: '3em',
         paddingBottom: '10px',
         sectionsColor : [$black, $white, $grey, $black],
-        fixedElements: '#header, .footer',
-        // responsiveWidth: 479,
-        // responsiveHeight: 600,
-
-        //Custom selectors
-        sectionSelector: '.section',
-        slideSelector: '.slide',
-
-        //events
-        onLeave: function(index, nextIndex, direction){},
-        afterLoad: function(anchorLink, index){},
-        afterRender: function(){
-          new WOW().init();
-        },
-        afterResize: function(){},
-        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
     }
 
 
